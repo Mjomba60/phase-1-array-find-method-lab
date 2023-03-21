1d1c5c52931f13a86dc5a791c532cf61a99a00f9
George Samuel <geosam2708@gmail.com>
2023-03-21 09:45:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find(({result}) => result === "W")
+    if (typeof result === "undefined"){
+        return undefined;
+    } else {
+        return result.year;
+    }
+
+}
